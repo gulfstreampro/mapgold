c21035fc7bc840b646ea4b775c3cbbde2b1e29a9
gulfstreampro <156713761+gulfstreampro@users.noreply.github.com>
2024-01-15 21:42:57 +0100

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "MMT-3D-ART.DIGITAL-1.0";
+const contentToCache = [
+    "Build/map gold.loader.js",
+    "Build/map gold.framework.js.unityweb",
+    "Build/map gold.data.unityweb",
+    "Build/map gold.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
